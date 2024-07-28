606d13bbdc072d21689be6a675ddac11e9d310ee
Sarosh Farhan <saroshfarhan6495@gmail.com>
2024-07-29 00:01:55 +0530

    Wip model

---
+++ src/components/Loader.jsx
@@ -0,0 +1,14 @@
+import { Html } from "@react-three/drei";
+import React from "react";
+
+const Loader = () => {
+  return (
+    <Html>
+      <div className="absolute left-0 top-0 flex h-full w-full items-center justify-center">
+        <div className="h-[10vw] w-[10vw] rounded-full">Loading...</div>
+      </div>
+    </Html>
+  );
+};
+
+export default Loader;
